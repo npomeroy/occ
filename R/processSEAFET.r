adf5156881c97de3a6bc2da6db9ad883f74774a9
Hannah Barkley <hannah.barkley@noaa.gov>
2018-05-17 17:59:23 -1000

    edits for salinity correction

--- R/processSEAFET.r
+++ R/processSEAFET.r
@@ -2,29 +2,54 @@ processSEAFET = function(file,
                          tz.in = "UTC",
                          tz.out = "UTC",
                          write.csv = TRUE,
-                         average = FALSE) {
+                         average = FALSE,
+                         corrected = TRUE) {
   # Read seaFET CSV data
   
-  test = read.csv(file, header = FALSE)
-  
-  skip = min(which(test[, 1] != "SATFHR")) - 1
-  
-  data = read.csv(file,
-                  header = FALSE,
-                  skip = skip)
-  
-  data$DateTime = ymd(strptime(data$V2, format = "%Y%j"), tz = tz.in) + seconds(data$V3 *
-                                                                                  3600)
-  
-  data$DateTime_tz.out = with_tz(data$DateTime, tz = tz.out)
-  
-  # Rename variables and add to data frame
-  date = data$DateTime_tz.out
-  pH_ext = data$V5
-  temp = data$V6
-  
-  processed = data.frame(date, pH_ext, temp)
-  colnames(processed) = c('DateTime', 'pH', 'Temp')
+  if (corrected == TRUE) {
+    test = read.csv(file, header = FALSE)
+    
+    skip = min(which(test[, 1] == "Frame Header")) + 1
+    
+                    
+    data = read.csv(file,
+                    header = FALSE,
+                    skip = skip)
+    
+    data$DateTime = dmy_hms(data$V2, tz = tz.in)
+    
+    data$DateTime_tz.out = with_tz(data$DateTime, tz = tz.out)
+    
+    # Rename variables and add to data frame
+    date = data$DateTime_tz.out
+    pH_ext = data$V4
+    temp = data$V5
+    
+    processed = data.frame(date, pH_ext, temp)
+    colnames(processed) = c('DateTime', 'pH', 'Temp')
+    
+  } else {
+    test = read.csv(file, header = FALSE)
+    
+    skip = min(which(test[, 1] != "SATFHR")) - 1
+    
+    data = read.csv(file,
+                    header = FALSE,
+                    skip = skip)
+    
+    data$DateTime = ymd(strptime(data$V2, format = "%Y%j"), tz = tz.in) + seconds(data$V3 *
+                                                                                    3600)
+    
+    data$DateTime_tz.out = with_tz(data$DateTime, tz = tz.out)
+    
+    # Rename variables and add to data frame
+    date = data$DateTime_tz.out
+    pH_ext = data$V5
+    temp = data$V6
+    
+    processed = data.frame(date, pH_ext, temp)
+    colnames(processed) = c('DateTime', 'pH', 'Temp')
+  }
   
   if (average == TRUE) {
     processed = processed %>%
